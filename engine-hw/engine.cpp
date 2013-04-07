62f410cfe944f3035bda4062130cc20d70ba2abb
Tero Hasu <tero.hasu@hut.fi>
2013-04-07 15:38:36 +0200

    engine skeleton

---
+++ engine-hw/engine.cpp
@@ -0,0 +1,10 @@
+#include "engine.hpp"
+
+#include "abstract.hpp"
+
+namespace engine
+{
+  void exportData() {
+  }
+} // end namespace engine
+
---
+++ engine-hw/engine.hpp
@@ -0,0 +1,13 @@
+#ifndef __engine_hpp__
+#define __engine_hpp__
+
+/** Handwritten version of the code that is later to be programmed
+    in another language. */
+namespace engine
+{
+  // xxx can take args for format and output
+  void exportData();
+}
+
+#endif /* __engine_hpp__ */
+
