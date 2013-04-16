25999b04284a5573215dfcf64cb0d06cc3bb3a67
Tero Hasu <tero.hasu@ii.uib.no>
2013-04-16 22:14:34 +0200

    fix GCC 4.4.5 issue with unique_ptr move
    was getting bad call error trying to call "null" function

--- src/abstract.cpp
+++ src/abstract.cpp
@@ -3,6 +3,10 @@
 namespace luastate
 {
   void init(LuaState& st) {
-    st = my_lua_newstate_smart();
+    // GCC 4.4.5 silently fails to assign if we try to assign.
+    // Let us explicitly swap.
+    // (Recent versions are okay with assign, too, realizing it
+    // is a move.)
+    st.swap(my_lua_newstate_smart());
   }
 }
