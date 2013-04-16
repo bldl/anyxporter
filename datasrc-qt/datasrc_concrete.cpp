7b26cb29a741670ebe351279d7ea7404b1b2c4a1
Tero Hasu <tero.hasu@ii.uib.no>
2013-04-16 23:25:00 +0200

    output field names of individual entries

--- datasrc-qt/datasrc_concrete.cpp
+++ datasrc-qt/datasrc_concrete.cpp
@@ -47,10 +47,28 @@ namespace datasrc
     iter.nextIx += 1;
   }
 
-  void entryToLua(Db const& db, Entry const& e, LuaState& st) {
+  void entryToLua(Db const& /*db*/, Entry const& e, LuaState& st) {
     lua_State* const L = st.get();
-    QMap<QString, QContactDetailDefinition> defs = db->detailDefinitions();
+    QList<QContactDetail> ds = e.details();
+    lua_createtable(L, 0, ds.size());
+    for (QList<QContactDetail>::const_iterator it = ds.constBegin(); 
+	 it != ds.constEnd();
+	 ++it) {
+      QContactDetail const& d = *it;
+      QString const n = d.definitionName();
+      QByteArray asciiKey = n.toAscii();
+      char const* const key = asciiKey.data();
+      QByteArray uniValue = n.toUtf8(); //xxx want value actually
+      char const* const value = uniValue.data();
+      lua_pushstring(L, value);
+      lua_setfield(L, -2, key);
+    }
+
+    /*
+      // Shows possible details, not entry details.
+    lua_State* const L = st.get();
     lua_createtable(L, 0, defs.size());
+    QMap<QString, QContactDetailDefinition> defs = db->detailDefinitions();
     QMap<QString, QContactDetailDefinition>::const_iterator it = 
       defs.constBegin();
     while (it != defs.constEnd()) {
@@ -62,6 +80,7 @@ namespace datasrc
       lua_setfield(L, -2, key);
       ++it;
     }
+    */
   }
 
 }
