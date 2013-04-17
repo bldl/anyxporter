ad86313b66829bcfc90bf990e1bb713103f3a9ff
Tero Hasu <tero.hasu@ii.uib.no>
2013-04-17 04:53:28 +0200

    getting quite a bit of actual data now
    but only one per definition name,
    maybe also lacking context info

--- datasrc-qt/datasrc_concrete.cpp
+++ datasrc-qt/datasrc_concrete.cpp
@@ -3,7 +3,9 @@
 #include "my_nullptr.hpp"
 
 #include <QDebug>
+
 #include <assert.h>
+#include <stdexcept>
 
 // Qt Mobility based data source.
 namespace datasrc
@@ -48,24 +50,51 @@ namespace datasrc
   }
 
   void entryToLua(Db const& /*db*/, Entry const& e, LuaState& st) {
+#if 1
+    lua_State* const L = st.get();
+    QList<QContactDetail> ds = e.details();
+    lua_createtable(L, 0, ds.size());
+    for (QList<QContactDetail>::const_iterator dit = ds.constBegin(); 
+	 dit != ds.constEnd(); ++dit) {
+      QContactDetail const& d = *dit;
+      if (d.isEmpty())
+	continue;
+      QMap<QString, QVariant> const vm = d.variantValues();
+      lua_createtable(L, 0, vm.size());
+      for (QMap<QString, QVariant>::const_iterator vit = vm.constBegin();
+	   vit != vm.constEnd(); ++vit) {
+	QString vn = vit.key(); // always in Latin1 range
+	QVariant vv = vit.value();
+	if (vv.canConvert<QString>()) {
+	  QString const s = vv.toString();
+	  lua_pushstring(L, s.toUtf8().data());
+	  lua_setfield(L, -2, vn.toUtf8().data());
+	} else {
+	  throw std::runtime_error
+	    (std::string("Unsupported type: ") + vv.typeName());
+	}
+      }
+      QString const dn = d.definitionName();
+      lua_setfield(L, -2, dn.toUtf8().data());
+    }
+#elif 0
+    // Shows entry definition names, not actual data.
     lua_State* const L = st.get();
     QList<QContactDetail> ds = e.details();
     lua_createtable(L, 0, ds.size());
     for (QList<QContactDetail>::const_iterator it = ds.constBegin(); 
-	 it != ds.constEnd();
-	 ++it) {
+	 it != ds.constEnd(); ++it) {
       QContactDetail const& d = *it;
       QString const n = d.definitionName();
-      QByteArray asciiKey = n.toAscii();
-      char const* const key = asciiKey.data();
-      QByteArray uniValue = n.toUtf8(); //xxx want value actually
-      char const* const value = uniValue.data();
+      QByteArray baKey = n.toUtf8(); // always in Latin1 range
+      char const* const key = baKey.data();
+      QByteArray baValue = n.toUtf8(); //xxx want value actually
+      char const* const value = baValue.data();
       lua_pushstring(L, value);
       lua_setfield(L, -2, key);
     }
-
-    /*
-      // Shows possible details, not entry details.
+#else
+    // Shows possible details, not entry details.
     lua_State* const L = st.get();
     lua_createtable(L, 0, defs.size());
     QMap<QString, QContactDetailDefinition> defs = db->detailDefinitions();
@@ -80,7 +109,7 @@ namespace datasrc
       lua_setfield(L, -2, key);
       ++it;
     }
-    */
+#endif
   }
 
 }
