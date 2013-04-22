94fd5e8b016e38786f9c6ad3ee782f9d7290ec80
Tero Hasu <tero.hasu@ii.uib.no>
2013-04-22 21:06:53 +0200

    make qt formatter support arrays of datums
    such as created from QStringList

--- lua-src/qt_contact_to_xml.lua
+++ lua-src/qt_contact_to_xml.lua
@@ -18,6 +18,30 @@ end
 
 --for k, v in ipairs(table_keys({a = 1, b = 2})) do print(v) end
 
+local function table_size (t)
+  local n = 0
+  for _, _ in pairs(t) do
+    n = n + 1
+  end
+  return n
+end
+
+--print(table_size({1, 3, 5}))
+
+local function is_array (x)
+  if type(x) ~= "table" then return nil end
+  local n = table_size(x)
+  local ni = 0
+  for _, _ in ipairs(x) do
+    ni = ni + 1
+  end
+  return n == ni
+end
+
+--print(is_array(true))
+--print(is_array({1, 3, 5}))
+--print(is_array({1, 3, a = 5}))
+
 preamble_string = [[<?xml version="1.0" encoding="UTF-8"?><Contacts>]]
 
 postamble_string = [[</Contacts>]]
@@ -26,12 +50,20 @@ local function datum_to_string (e)
   if type(e) == "string" then
     return xml_esc(e)
   elseif type(e) == "table" then
-    local s = ""
-    local ks = table_keys(e)
-    for i, k in ipairs(ks) do
-      s = s .. string.format([[<%s>%s</%s>]], k, datum_to_string(e[k]), k)
+    if is_array(e) then
+      local s = ""
+      for _, v in ipairs(e) do
+	s = s .. string.format([[<element>%s</element>]], v)
+      end
+      return s
+    else
+      local s = ""
+      local ks = table_keys(e)
+      for i, k in ipairs(ks) do
+	s = s .. string.format([[<%s>%s</%s>]], k, datum_to_string(e[k]), k)
+      end
+      return s
     end
-    return s
   else
     error("unsupported type " + type(e))
   end
