c7f1d1dea71d91d99c62b391dbe6e1b63600d6bb
Tero Hasu <tero.hasu@ii.uib.no>
2013-04-23 23:11:33 +0200

    an uploading variant for harmattan - does not build, though
    would need to avoid using the new http multipart api

---
+++ variants/harmattan-console-mock-http.var.rkt
@@ -0,0 +1,39 @@
+#lang racket
+
+#|
+
+A Qt-based console program that exports mock data, with an option to
+do HTTP uploading. Targeting Harmattan.
+
+This configuration is actually not valid, as Harmattan has Qt 4.7.4,
+and hence lacks QHttpPart.
+
+|#
+
+(require "base.rkt")
+(require "konffaa/util.rkt")
+(require "konffaa/variant.rkt")
+
+(require (only-in "harmattan-console.var.rkt" (klass% parent%)))
+
+(define* klass%
+   (variant-class
+    parent%
+    (super-new)
+    
+    (define/override (with-qt-mobility.attr)
+      #f)
+  
+    (define/override (feature-http-post.attr)
+      #t)
+    
+    (define/override (lua-export-script.attr)
+      "contact_to_xml.lua")
+
+    (define/override (component-datasrc.attr)
+      'datasrc-mock)
+
+    )) ;; end class
+
+(define* (info)
+  (new klass%)) 
